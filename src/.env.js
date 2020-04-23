a27d1ffcc6062394929b7c646304343aab058f2f
Ramjan ali <me.ramjanali@gmail.com>
2020-04-24 01:36:12 +0600

    add style css back

--- src/.env.js
+++ src/.env.js
@@ -1,3 +1,3 @@
 export const API_PORT = '4000'
 export const API_URL = `http://localhost:${API_PORT}`
-export const DOMAIN_TITLE = 'supersite.com'
+export const DOMAIN_TITLE = 'Covid-19 world update'
